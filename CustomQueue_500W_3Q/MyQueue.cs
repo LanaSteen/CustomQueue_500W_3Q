9a27092c291775a525ccebf16e6d805d968c796c
l4n.steen@gmail.com <l4n.steen@gmail.com>
2022-11-12 13:30:19 +0400

    Add project files.

---
+++ CustomQueue_500W_3Q/MyQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace CustomQueue_500W_3Q
+{
+    public class MyQueue<T>
+    {
+        public List<T> _queue;
+       
+        public MyQueue(List<T> queue)
+        {
+            _queue = queue;
+        }
+        public MyQueue()
+        {
+            _queue = new List<T>();
+        }
+
+        public T Dequeue()
+        {
+            T item = default(T);
+
+
+            if (_queue.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Queue is empty");
+            }
+            else
+            {
+                item = _queue[_queue.Count - 1];
+                _queue.RemoveAt(_queue.Count - 1);
+            }
+
+            return (item);
+        }
+
+        public void Enqueue(T item)
+        {
+
+            _queue.Add(item);
+
+        }
+
+
+        public int Size()
+        {
+
+            return _queue.Count;
+        }
+
+        public T PeekQueue()
+        {
+            if (_queue.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Queue is empty");
+            }
+            return _queue[0];
+
+
+        }
+        public void ClearQueue()
+        {
+
+            _queue.Clear();
+
+        }
+       
+    }
+}
---
+++ CustomQueue_500W_3Q/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomQueue_500W_3Q
+{   /*Შექმენით Custom Queue- ჯენერიკ იმპლემენტიაცია განუსაზღვრეთ კონსტრუქტორი პარამეტრიანი და უპარამეტროც.
+     Უნდა შეიძებოდეს ქიუს ზომის შემოწმება ცარიელია თუ არა. 
+     Უნდა იყოს ელემენტის დამატების და წაშლის მეთოდები. 
+     Დანარჩენი მეთოდები შეგიძლიათ დაამატოთ სურვილისამებრ.*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyQueue<int> queue = new MyQueue<int>();
+            queue.Enqueue(10);
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+            queue.Enqueue(40);
+            queue.Enqueue(-50);
+
+
+            Console.WriteLine(queue.PeekQueue());
+            Console.WriteLine(queue.Size());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Size());
+            queue.ClearQueue();
+            Console.WriteLine(queue.Size());
+        }
+    }
+}
